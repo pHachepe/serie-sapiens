a4a4bf6497bdb12c276693a3dece795615c00464
PeHachePe <pehachepe@uoc.edu>
2023-12-17 16:07:51 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')
+    , require('@tailwindcss/forms')
+    , require('@tailwindcss/line-clamp')
+    , require('@tailwindcss/typography')
+  ],
+};
