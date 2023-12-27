9260b81301ac524f123c50cc89d664886c3270c9
PeHachePe <pehachepe@uoc.edu>
2023-12-27 01:15:55 +0100

    add result data service

---
+++ src/app/services/result-data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Result } from '../models/result.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ResultDataService {
+  private selectedResult: Result | null = null;
+
+  setSelectedResult(result: Result) {
+    this.selectedResult = result;
+  }
+
+  getSelectedResult(): Result {
+    return this.selectedResult!;
+  }
+}
