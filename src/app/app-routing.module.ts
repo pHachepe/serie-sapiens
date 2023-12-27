2e7d9dcf6db211fe2d585b5d6f2787c8506f8f81
PeHachePe <pehachepe@uoc.edu>
2023-12-27 01:16:17 +0100

    upd routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ResourceDetailsPage } from './pages/resource-details/resource-details.page';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },
+  {
+    path: 'resource-details',
+    loadChildren: () =>
+      import('./pages/resource-details/resource-details.module').then(
+        (m) => m.ResourceDetailsPageModule
+      ),
+  },
+  { path: 'resource-details/:media_type/:id', component: ResourceDetailsPage },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
