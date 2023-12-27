60d3dc84ef0c246bbcc45a1a662366ba28b89c6d
PeHachePe <pehachepe@uoc.edu>
2023-12-27 01:10:12 +0100

    upd movie card comp

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,18 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
+import { Router } from '@angular/router';
+import { ResultDataService } from 'src/app/services/result-data.service';
+import { Result } from 'src/app/models/result.model';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss'],
 })
-export class MovieCardComponent implements OnInit {
-  @Input() movie: any;
+export class MovieCardComponent {
+  @Input() result!: Result;
 
-  constructor() {}
+  constructor(
+    private router: Router,
+    private resultDataService: ResultDataService
+  ) {}
 
-  ngOnInit() {}
-
-  goToFilmDetail() {
-    console.log('goToFilmDetail: ', this.movie);
+  goToDetail() {
+    this.resultDataService.setSelectedResult(this.result);
+    this.router.navigate(['/resource-details', this.result.media_type, this.result.id]);
   }
 }
+
+@NgModule({
+  declarations: [MovieCardComponent],
+  imports: [IonicModule, CommonModule],
+  exports: [MovieCardComponent],
+})
+export class MovieCardModule {}
