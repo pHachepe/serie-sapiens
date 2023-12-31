45a249a0fcad0266694aaeb9b617fc90f9c66080
PeHachePe <pehachepe@uoc.edu>
2023-12-31 02:21:05 +0100

    add interfaces videos

---
+++ src/app/models/video.model.ts
@@ -0,0 +1,19 @@
+export interface Video {
+  id: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  key: string;
+  name: string;
+  official: boolean;
+  published_at: string;
+  site: string;
+  size: number;
+  type: string;
+
+  url: string;
+}
+
+export interface Videos {
+  id: number;
+  results: Video[];
+}
