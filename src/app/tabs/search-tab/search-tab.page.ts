90bb5319efbcad3939d939e318aaf4eb7a5deccf
PeHachePe <pehachepe@uoc.edu>
2023-12-31 18:03:36 +0100

    add search tab

---
+++ src/app/tabs/search-tab/search-tab.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-tab',
+  templateUrl: './search-tab.page.html',
+})
+export class SearchTabPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
