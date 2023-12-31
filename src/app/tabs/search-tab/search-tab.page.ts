61186ebfa861d144bc04e7e1870e735027ac2ba2
PeHachePe <pehachepe@uoc.edu>
2023-12-31 19:21:27 +0100

    add search tab first version

--- src/app/tabs/search-tab/search-tab.page.ts
+++ src/app/tabs/search-tab/search-tab.page.ts
@@ -1,11 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MoviedbService } from 'src/app/api/moviedb.service';
+import { ApiResult } from 'src/app/models/result.model';
 
 @Component({
   selector: 'app-search-tab',
   templateUrl: './search-tab.page.html',
 })
 export class SearchTabPage implements OnInit {
-  constructor() {}
+  searchQuery: string = '';
+  searchResults: ApiResult | null = null;
+  isLoading = false;
+
+  constructor(private router: Router, private moviedbService: MoviedbService) {}
 
   ngOnInit() {}
+
+  onSearch2(query: string | null = null) {
+    if (!query) return; // Evita búsquedas vacías
+
+    this.isLoading = true;
+    this.moviedbService.search(query).subscribe(
+      (results) => {
+        this.searchResults = results;
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Error al realizar la búsqueda:', error);
+        this.isLoading = false;
+      }
+    );
+  }
+
+  onSearch() {
+    if (!this.searchQuery.trim()) {
+      this.searchResults = null;
+      return;
+    }
+
+    this.isLoading = true;
+    this.moviedbService.search(this.searchQuery).subscribe(
+      (results) => {
+        this.searchResults = results;
+        this.isLoading = false;
+        console.log(results);
+      },
+      (error) => {
+        console.error('Error al realizar la búsqueda:', error);
+        this.isLoading = false;
+      }
+    );
+  }
+
+  openDetails(item: any) {
+    this.router.navigate(['/tabs/details', item.media_type, item.id]);
+  }
 }
