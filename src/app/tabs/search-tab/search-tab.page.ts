075c93f8adbf54c313303024170c5227b53a3cff
PeHachePe <pehachepe@uoc.edu>
2024-01-02 03:31:48 +0100

    search page

--- src/app/tabs/search-tab/search-tab.page.ts
+++ src/app/tabs/search-tab/search-tab.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { MoviedbService } from 'src/app/api/moviedb.service';
 import { ApiResult } from 'src/app/models/result.model';
 
@@ -11,45 +13,38 @@ export class SearchTabPage implements OnInit {
   searchQuery: string = '';
   searchResults: ApiResult | null = null;
   isLoading = false;
+  private searchSubject = new Subject<string>();
 
   constructor(private router: Router, private moviedbService: MoviedbService) {}
 
-  ngOnInit() {}
-
-  onSearch2(query: string | null = null) {
-    if (!query) return; // Evita búsquedas vacías
-
-    this.isLoading = true;
-    this.moviedbService.search(query).subscribe(
-      (results) => {
-        this.searchResults = results;
-        this.isLoading = false;
-      },
-      (error) => {
-        console.error('Error al realizar la búsqueda:', error);
-        this.isLoading = false;
-      }
-    );
+  ngOnInit() {
+    this.searchSubject
+      .pipe(
+        debounceTime(500), // Espera 500ms después de cada cambio
+        distinctUntilChanged(), // Solo emite si el valor actual es diferente al anterior
+        switchMap((query) => {
+          this.isLoading = true;
+          if (!query.trim()) {
+            this.isLoading = false;
+            return []; // Evita búsquedas vacías
+          }
+          return this.moviedbService.search(query);
+        })
+      )
+      .subscribe(
+        (results) => {
+          this.searchResults = results;
+          this.isLoading = false;
+        },
+        (error) => {
+          console.error('Error al realizar la búsqueda:', error);
+          this.isLoading = false;
+        }
+      );
   }
 
-  onSearch() {
-    if (!this.searchQuery.trim()) {
-      this.searchResults = null;
-      return;
-    }
-
-    this.isLoading = true;
-    this.moviedbService.search(this.searchQuery).subscribe(
-      (results) => {
-        this.searchResults = results;
-        this.isLoading = false;
-        console.log(results);
-      },
-      (error) => {
-        console.error('Error al realizar la búsqueda:', error);
-        this.isLoading = false;
-      }
-    );
+  onSearchChange() {
+    this.searchSubject.next(this.searchQuery);
   }
 
   openDetails(item: any) {
