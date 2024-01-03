03c5932b49f7a65e48ee1d88abe15232f16410c1
afontana <afontana@unedbarbastro.es>
2024-01-03 13:09:07 +0100

    upd header

--- src/app/tabs/search-tab/search-tab.module.ts
+++ src/app/tabs/search-tab/search-tab.module.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { HeaderModule } from 'src/app/components/header/header.component';
 import { SearchTabPageRoutingModule } from './search-tab-routing.module';
-
 import { SearchTabPage } from './search-tab.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, SearchTabPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HeaderModule,
+    IonicModule,
+    SearchTabPageRoutingModule,
+  ],
   declarations: [SearchTabPage],
 })
-export class SearchTabPageModule {}
+export class SearchTabPageModule { }
--- src/app/tabs/settings-tab/settings-tab.module.ts
+++ src/app/tabs/settings-tab/settings-tab.module.ts
@@ -1,12 +1,19 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { SettingsTabPage } from './settings-tab.page';
+import { IonicModule } from '@ionic/angular';
+import { HeaderModule } from 'src/app/components/header/header.component';
 import { SettingsTabPageRoutingModule } from './settings-tab-routing.module';
+import { SettingsTabPage } from './settings-tab.page';
 
 @NgModule({
-  imports: [IonicModule, CommonModule, FormsModule, SettingsTabPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HeaderModule,
+    IonicModule,
+    SettingsTabPageRoutingModule,
+  ],
   declarations: [SettingsTabPage],
 })
-export class SettingsTabPageModule {}
+export class SettingsTabPageModule { }
