3e5ef558e5364328415d557ad59b8f994a8abaa4
afontana <afontana@unedbarbastro.es>
2024-01-03 13:06:15 +0100

    upd header

--- src/app/pages/resource-details/resource-details.module.ts
+++ src/app/pages/resource-details/resource-details.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
-import { ResourceDetailsPageRoutingModule } from './resource-details-routing.module';
-
-import { ResourceDetailsPage } from './resource-details.page';
+import { HeaderModule } from 'src/app/components/header/header.component';
 import { SwiperModule } from 'src/app/components/swiper/swiper.component';
+import { ResourceDetailsPageRoutingModule } from './resource-details-routing.module';
+import { ResourceDetailsPage } from './resource-details.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    HeaderModule,
     IonicModule,
     ResourceDetailsPageRoutingModule,
-    SwiperModule
+    SwiperModule,
   ],
   declarations: [ResourceDetailsPage]
 })
-export class ResourceDetailsPageModule {}
+export class ResourceDetailsPageModule { }
