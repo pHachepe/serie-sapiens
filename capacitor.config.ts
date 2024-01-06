24c08bc8be19d6c2b90a5701f4cec3c558181d43
PeHachePe <pehachepe@uoc.edu>
2024-01-07 00:36:30 +0100

    upd config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.seriesapiens.app',
   appName: 'SerieSapiens',
   webDir: 'www',
   server: {
