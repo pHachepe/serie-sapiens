187539a19b7672b7f80804be1ab5be5a85f29bda
PeHachePe <pehachepe@uoc.edu>
2024-01-06 17:10:54 +0100

    add sapiens tab

--- src/app/tabs/sapiens-tab/sapiens-tab.module.ts
+++ src/app/tabs/sapiens-tab/sapiens-tab.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HeaderModule } from 'src/app/components/header/header.component';
+import { SwiperModule } from 'src/app/components/swiper/swiper.component';
 import { SapiensTabPageRoutingModule } from './sapiens-tab-routing.module';
 import { SapiensTabPage } from './sapiens-tab.page';
 
@@ -12,8 +13,9 @@ import { SapiensTabPage } from './sapiens-tab.page';
     FormsModule,
     HeaderModule,
     IonicModule,
-    SapiensTabPageRoutingModule
+    SapiensTabPageRoutingModule,
+    SwiperModule,
   ],
-  declarations: [SapiensTabPage]
+  declarations: [SapiensTabPage],
 })
-export class SapiensTabPageModule { }
+export class SapiensTabPageModule {}
--- src/app/tabs/sapiens-tab/sapiens-tab.page.ts
+++ src/app/tabs/sapiens-tab/sapiens-tab.page.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { forkJoin, of, switchMap } from 'rxjs';
+import { GptApiService } from 'src/app/api/gpt-api.service';
+import { MoviedbService } from 'src/app/api/moviedb.service';
+import { MT } from 'src/app/models/gpt-response.model';
 import { Result } from 'src/app/models/result.model';
 
 interface ChatMessage {
   sender: 'user' | 'app';
   text: string;
+  searchResults?: Result[];
 }
 
 @Component({
   selector: 'app-sapiens-tab-page',
   templateUrl: './sapiens-tab.page.html',
-  styleUrls: ['./sapiens-tab.page.scss'],
 })
 export class SapiensTabPage {
   searchResults: Result[] = [];
@@ -17,52 +21,55 @@ export class SapiensTabPage {
   searchText: string = '';
 
   private welcomeMessages: string[] = [
-    "Hola, ¿qué película buscas hoy?",
-    "Bienvenido, dime el título de la película que quieres ver.",
-    "¿Listo para una noche de cine? ¿Qué te apetece?",
-    "Encantado de ayudarte a encontrar la película perfecta.",
-    "Un mundo de películas te espera, ¿por cuál empezamos?",
-    "Cuéntame qué tipo de películas te gustan.",
-    "¡Explorando nuevas películas! ¿Qué buscas hoy?",
-    "Vamos a encontrar algo emocionante para ver.",
-    "¿Buscas algo específico o quieres sugerencias?",
-    "¿Qué género de películas prefieres?",
-    "Pregunta por cualquier película, ¡estoy aquí para ayudar!",
-    "¿Hay alguna película que te interese ver hoy?",
-    "Listo para recomendarte las mejores películas.",
-    "Cuéntame más sobre tus gustos cinematográficos.",
-    "¿Qué te gustaría explorar en el mundo del cine hoy?",
-    "Dime el nombre de una película o un actor y te ayudaré.",
-    "¿Alguna preferencia para la película de hoy?",
-    "Aquí para ayudarte a encontrar tu próxima película favorita.",
-    "Vamos a encontrar la película perfecta para esta noche.",
-    "¿Buscas un clásico o algo nuevo?"
+    'Hola, ¿qué película buscas hoy?',
+    'Bienvenido, dime el título de la película que quieres ver.',
+    '¿Listo para una noche de cine? ¿Qué te apetece?',
+    'Encantado de ayudarte a encontrar la película perfecta.',
+    'Un mundo de películas te espera, ¿por cuál empezamos?',
+    'Cuéntame qué tipo de películas te gustan.',
+    '¡Explorando nuevas películas! ¿Qué buscas hoy?',
+    'Vamos a encontrar algo emocionante para ver.',
+    '¿Buscas algo específico o quieres sugerencias?',
+    '¿Qué género de películas prefieres?',
+    'Pregunta por cualquier película, ¡estoy aquí para ayudar!',
+    '¿Hay alguna película que te interese ver hoy?',
+    'Listo para recomendarte las mejores películas.',
+    'Cuéntame más sobre tus gustos cinematográficos.',
+    '¿Qué te gustaría explorar en el mundo del cine hoy?',
+    'Dime el nombre de una película o un actor y te ayudaré.',
+    '¿Alguna preferencia para la película de hoy?',
+    'Aquí para ayudarte a encontrar tu próxima película favorita.',
+    'Vamos a encontrar la película perfecta para esta noche.',
+    '¿Buscas un clásico o algo nuevo?',
   ];
 
   private responseMessages: string[] = [
-    "Déjame buscar eso para ti.",
-    "Aquí tienes algunas opciones que podrían interesarte.",
-    "Un momento, estoy encontrando las mejores coincidencias.",
-    "Estas películas podrían ser justo lo que buscas.",
-    "He encontrado algo que podría gustarte.",
-    "Basado en tu búsqueda, estas son mis recomendaciones.",
-    "Echa un vistazo a estos títulos.",
-    "Creo que estas películas se ajustan a lo que buscas.",
-    "Déjame mostrarte lo que he encontrado.",
-    "Espero que encuentres interesantes estas sugerencias.",
-    "Aquí están las películas que coinciden con tu búsqueda.",
-    "He seleccionado algunas películas que podrían gustarte.",
-    "Estos son los resultados que he encontrado para ti.",
-    "Creo que estas opciones podrían ser de tu agrado.",
-    "Echa un vistazo a estas películas y dime qué opinas.",
-    "Estas son las películas que he encontrado basadas en tu petición.",
-    "Espero que alguna de estas películas sea lo que buscas.",
-    "Aquí tienes algunos títulos que coinciden con tus criterios.",
-    "Estos son los resultados basados en tu interés.",
-    "He aquí algunas películas que podrían interesarte."
+    'Déjame buscar eso para ti.',
+    'Aquí tienes algunas opciones que podrían interesarte.',
+    'Un momento, estoy encontrando las mejores coincidencias.',
+    'Estas películas podrían ser justo lo que buscas.',
+    'He encontrado algo que podría gustarte.',
+    'Basado en tu búsqueda, estas son mis recomendaciones.',
+    'Echa un vistazo a estos títulos.',
+    'Creo que estas películas se ajustan a lo que buscas.',
+    'Déjame mostrarte lo que he encontrado.',
+    'Espero que encuentres interesantes estas sugerencias.',
+    'Aquí están las películas que coinciden con tu búsqueda.',
+    'He seleccionado algunas películas que podrían gustarte.',
+    'Estos son los resultados que he encontrado para ti.',
+    'Creo que estas opciones podrían ser de tu agrado.',
+    'Echa un vistazo a estas películas y dime qué opinas.',
+    'Estas son las películas que he encontrado basadas en tu petición.',
+    'Espero que alguna de estas películas sea lo que buscas.',
+    'Aquí tienes algunos títulos que coinciden con tus criterios.',
+    'Estos son los resultados basados en tu interés.',
+    'He aquí algunas películas que podrían interesarte.',
   ];
 
-  constructor() {
+  constructor(
+    private gptApiService: GptApiService,
+    private moviedbService: MoviedbService
+  ) {
     const randomWelcome = this.getRandomMessage(this.welcomeMessages);
     this.chatMessages.push({ sender: 'app', text: randomWelcome });
   }
@@ -72,11 +79,55 @@ export class SapiensTabPage {
 
     this.chatMessages.push({ sender: 'user', text: this.searchText });
 
-    const mockResults = this.getMockResults(); // Obtener resultados ficticios
-    this.searchResults = mockResults;
+    this.gptApiService
+      .getApiResponseChatGPTtopicFromServer(this.searchText)
+      .pipe(
+        switchMap((results) => {
+          // Filtrar para encontrar el resultado con "ot": "reply" y quitarlo de la lista
+          const reply = results.find(
+            (result) => result.mt === MT.Reply || result.ot === 'reply'
+          );
+          results = results.filter(
+            (result) => result.mt !== MT.Reply && result.ot !== 'reply'
+          );
 
-    const randomResponse = this.getRandomMessage(this.responseMessages);
-    this.chatMessages.push({ sender: 'app', text: randomResponse });
+          // Crear un array de Observables para cada búsqueda
+          const searchObservables = results.map((result) =>
+            this.moviedbService.search(result.ot).pipe(
+              switchMap((apiResult) => {
+                const firstResult = apiResult.results[0];
+                return firstResult ? of(firstResult) : of(null);
+              })
+            )
+          );
+
+          // Retorna un Observable que emite un array de todos los resultados
+          return forkJoin(searchObservables).pipe(
+            switchMap((searchResults) => {
+              // Filtrar los resultados nulos y devolver los resultados junto con el 'reply'
+              return of({
+                searchResults: searchResults.filter((res) => res !== null),
+                reply,
+              });
+            })
+          );
+        })
+      )
+      .subscribe(({ searchResults, reply }) => {
+        const randomResponse = this.getRandomMessage(this.responseMessages);
+
+        // Filtrar los resultados para excluir los valores null
+        const validSearchResults = searchResults.filter(
+          (result) => result !== null
+        ) as Result[];
+
+        // Añadir un mensaje con los resultados de búsqueda
+        this.chatMessages.push({
+          sender: 'app',
+          text: reply?.message || randomResponse,
+          searchResults: validSearchResults,
+        });
+      });
 
     this.searchText = ''; // Restablecer campo de búsqueda
   }
@@ -84,28 +135,5 @@ export class SapiensTabPage {
   private getRandomMessage(messages: string[]): string {
     const randomIndex = Math.floor(Math.random() * messages.length);
     return messages[randomIndex];
-  }
-
-  private getMockResults(): Result[] {
-    // Generar resultados ficticios
-    return [
-      {
-        adult: false,
-        backdrop_path: '/path/to/image1.jpg',
-        genre_ids: [28, 12],
-        id: 1,
-        original_language: 'en',
-        original_title: 'The Adventure Begins',
-        overview: 'Una aventura épica llena de emoción y descubrimiento.',
-        popularity: 8.7,
-        poster_path: '/path/to/poster1.jpg',
-        release_date: '2024-01-01',
-        title: 'The Adventure Begins',
-        name: 'The Adventure Begins',
-        vote_average: 7.5,
-        vote_count: 215,
-        first_air_date: '2024-01-01',
-      },
-    ];
   }
 }
