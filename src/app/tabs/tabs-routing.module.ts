db944e360b44f134eade7cca9488ac11c1f87bed
PeHachePe <pehachepe@uoc.edu>
2024-01-06 17:11:42 +0100

    upd first screen

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -5,7 +5,7 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/tabs/trendingtab',
+    redirectTo: '/tabs/sapienstab',
     pathMatch: 'full',
   },
   {
